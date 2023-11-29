b3893e7a9a7b9eaa85a4366e75b1ce3cb7dad323
Michael Gillett <51103663+migillett@users.noreply.github.com>
2023-11-29 14:26:41 -0500

    mods to bash scripts

--- record.sh
+++ record.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-DEVICE='DeckLink Mini Recorder 4K'
-FORMAT='hp59'
+# https://forum.blackmagicdesign.com/viewtopic.php?f=12&t=50941
+
 # hp59 = 720p59.96
 # replace hp59 with whatever format you like from:
 # ffmpeg -f decklink -list_formats 1 -i '$DEVICE'
 
-ffmpeg -format_code $FORMAT -f decklink -i '$DEVICE' -c:v libx264 -b:v 2000k output.mp4
+
+DEVICE='DeckLink Mini Recorder 4K'
+FORMAT='Hp59'
+PORT='hdmi'
+
+ffprobe -f decklink -i '$DEVICE' -select_streams v:0 -video_input $PORT -audio_input embedded
+
+ffmpeg -y -format_code $FORMAT -f decklink -video_input $PORT -audio_input embedded -raw_format argb -i '$DEVICE' -c:v libx264 -b:v 2000k /mnt/network_storage/out.mp4
