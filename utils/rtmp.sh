82a4933e8fbd8fe5a09123e8d6c6adc2700e6ecd
Michael Gillett <51103663+migillett@users.noreply.github.com>
2023-11-30 23:43:37 -0500

    rtmp fixes

--- utils/rtmp.sh
+++ utils/rtmp.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 DEVICE="DeckLink Mini Recorder 4K"
-FORMAT="Hp59"
+FORMAT="hp60"
 INPUT_TYPE="hdmi"
-RTMP_URL="" # rtmp://a.rtmp.youtube.com/live2
+RTMP_URL=""
 
 ffmpeg \
-	-format_code $FORMAT \
-	-f decklink \
-	-video_input $INPUT_TYPE \
-	-audio_input embedded \
-	-raw_format argb \
-	-i "$DEVICE" \
-	-c:v libx264 \
-	-c:a aac \
- 	-preset superfast \
-	-f flv \
-	"${RTMP_URL}"
+        -format_code $FORMAT \
+        -f decklink \
+        -video_input $INPUT_TYPE \
+        -audio_input embedded \
+        -raw_format argb \
+        -i "$DEVICE" \
+        -c:v libx264 \
+        -preset medium \
+        -pix_fmt yuv420p \
+        -profile:v main \
+        -c:a aac \
+        -shortest \
+        -f flv \
+        "${RTMP_URL}"
